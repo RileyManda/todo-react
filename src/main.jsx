f3a4c9f14a5cbff8fcea26941623c24365409b4f
RileyManda <rileymanda69@gmail.com>
2023-07-18 10:34:56 +0200

    Bug fix in project setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import TodoApp from '@/components/TodoApp';
+import TodoApp from './components/TodoApp';
 
 import '@/styles/app.css';
 // import './styles/app.css';
